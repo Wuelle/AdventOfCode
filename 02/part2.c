34a507d33cbb9424fa30c8afb295c873a5a40e5b
Simon Wülker <simon.wuelker@arcor.de>
2021-12-02 23:59:18 +0100

    solve second puzzle

---
+++ 02/part2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_OP_SIZE 10
+
+int main() {
+    int val;
+    char op[MAX_OP_SIZE];
+    int depth = 0;
+    int pos = 0;
+    int aim = 0;
+
+    FILE *fp;
+
+    // Open the input file
+    fp = fopen("input.txt", "r");
+    if(fp == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    // Read the file line by line
+    while(fscanf(fp, "%s %d\n", op, &val) != EOF) {
+        if(strcmp(op, "forward") == 0) {
+            pos += val;
+            depth += aim * val;
+        }
+        else if(strcmp(op, "up") == 0) {
+            aim -= val;
+        }
+        else if(strcmp(op, "down") == 0) {
+            aim += val;
+        }
+    }
+    printf("%d\n", depth * pos);
+
+    fclose(fp);
+    exit(EXIT_SUCCESS);
+}
